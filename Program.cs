c06f762053d82132bc12fcc89bf061ad51fb4b3e
filipe <filipe@Debian-11.lan>
2023-06-01 22:14:13 +0100

    [CV] Add custom page and remove unused stuff

--- Program.cs
+++ Program.cs
@@ -13,7 +13,6 @@ internal class Program
         builder.RootComponents.Add<HeadOutlet>("head::after");
 
         builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-        // builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:8123") });
         // builder.Services.AddBlazorBootstrap(); // Add this line
         builder.Services.AddMudServices();
 
